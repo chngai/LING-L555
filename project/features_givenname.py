468886c130b6f51d121c9f7112cd00ab99e30bbe
chngai <chngai@iu.edu>
2021-12-15 09:47:33 -0500

    updating stats_givenname.py and feature_givenname.py

--- project/features_givenname.py
+++ project/features_givenname.py
@@ -31,39 +31,39 @@ for i in df['pinyin']:
 	if '1' in i:
 		#tone 1 is a high level tone
 		tones.append(1)
-		heights.append(3)
-		contours.append(1)
+		heights.append('high')
+		contours.append('level')
 		levels.append(1)
 		risings.append(0)
 		fallings.append(0)
-		melodies.append(2)
+		melodies.append('high')
 	elif '2' in i:
 		#tone 2 is mid rising tone
 		tones.append(2)
-		heights.append(2)
-		contours.append(2)
+		heights.append('mid')
+		contours.append('rising')
 		levels.append(0)
 		risings.append(1)
 		fallings.append(0)
-		melodies.append(2)
+		melodies.append('high')
 	elif '3' in i:
 		#tone 3 is low falling-rising tone
 		tones.append(3)
-		heights.append(1)
-		contours.append(3)
+		heights.append('low')
+		contours.append('falling-rising')
 		levels.append(0)
 		risings.append(1)
 		fallings.append(1)
-		melodies.append(1)
+		melodies.append('low')
 	elif '4' in i:
 		#tone 4 is high falling tone
 		tones.append(4)
-		heights.append(3)
-		contours.append(4)
+		heights.append('high')
+		contours.append('falling')
 		levels.append(0)
 		risings.append(0)
 		fallings.append(1)
-		melodies.append(1)
+		melodies.append('low')
 	elif re.findall(r'[^1234]', i):
 		tones.append('missing')
 		heights.append('missing')
@@ -81,6 +81,38 @@ df['tone_rising'] =risings
 df['tone_falling'] =fallings
 df['tone_melody'] = melodies
 
+
+#Tone Update
+registers =[]
+pitches = []
+melodies = []
+
+for i in df ['pinyin']:
+	if '1' in i:
+		registers.append('+upper')
+		pitches.append ('raised_H')
+		melodies.append('high')
+	elif '2' in i:
+		registers.append('+upper')
+		pitches.append ('raised_L')
+		melodies.append('high')
+	elif '3' in i:
+		registers.append('-upper')
+		pitches.append ('raised_H')
+		melodies.append('low')
+	elif '4' in i:
+		registers.append('-upper')
+		pitches.append ('raised_L')
+		melodies.append('low')
+	elif re.findall(r'[^1234]', i):
+		registers.append('missing')
+		pitches.append ('missing')
+		melodies.append('missing')
+
+df['tone_register'] = registers
+df['tone_pitch'] = pitches
+df['tone_melody'] = melodies
+
 #---------------------------------this is a line-----------------------------------------
 #converting vowels features to numerics
 
@@ -92,22 +124,22 @@ roundednesses =[]
 #backedness feature for monophthong
 for i in df['ipa']:
 	if re.findall (r'[^aəiyɤuoe]*[iye][^aəiyɤuoe]*', i):    #i, e, and y
-		backednesses.append(1)
+		backednesses.append('front')
 	elif re.findall (r'[^aəiyɤuoe]*[əa][^aəiyɤuoe]*', i):  #ə and a(isn't a front?)
-		backednesses.append(2) 
+		backednesses.append('center') 
 	elif re.findall (r'[^aəiyɤuoe]*[uoɤ][^aəiyɤuoe]*', i):   # u, o, and ɤ
-		backednesses.append(3)
+		backednesses.append('back')
 	else: 
 		backednesses.append('missing')
 
 #height feature for monophthong
 for i in df['ipa']:
 	if re.findall (r'[^aəiyɤuoe]*[iyuɤ][^aəiyɤuoe]*', i):  #i, y, u, and ɤ
-		heights.append(3)
+		heights.append('high')
 	elif re.findall (r'[^aəiyɤuoe]*[əeo][^aəiyɤuoe]*', i):    #ə,o, and e
-		heights.append(2) 
+		heights.append('middle') 
 	elif re.findall (r'[^aəiyɤuoe]*[a][^aəiyɤuoe]*', i):    #a
-		heights.append(1)
+		heights.append('low')
 	else: #fail safe for missing vowel for height feature
 		heights.append('missing')
 
@@ -115,9 +147,9 @@ for i in df['ipa']:
 #rounded feature
 for i in df['ipa']:
 	if re.findall (r'[^aəiyɤuoe]*[uoy][^aəiyɤuoe]*', i):      #u, o, and y
-		roundednesses.append(1)
+		roundednesses.append('rounded')
 	elif re.findall (r'[^aəiyɤuoe]*[iɤəae][^aəiyɤuoe]*', i):   #i, ɤ, ə, e, and a
-		roundednesses.append(0)
+		roundednesses.append('unround')
 	else:            #fail safe for missing vowel for height feature
 		roundednesses.append('missing')
 		
